71183cb2158a72a0cc753b070ebc5e52bcf9f228
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2021-06-30 14:54:02 +0600

    Update context_processors.py

--- income/context_processors.py
+++ income/context_processors.py
@@ -1,15 +1,13 @@
 from .models import Income
-
-
 from django.db.models import Avg, Sum, Max, Min, Count
 
 
-def getIncomeData(request):
+# def getIncomeData(request):
 
-    income = Income.objects.filter(user=request.user).aggregate(Sum("amount"))
+#     income = Income.objects.filter(user=request.user).aggregate(Sum("amount"))
 
-    context = {
-        "totolIncome": income["amount__sum"],
-    }
+#     context = {
+#         "totolIncome": income["amount__sum"],
+#     }
 
-    return context
+#     return context
