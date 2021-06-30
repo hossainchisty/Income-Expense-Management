498e0826ebc32979ff5185d444f769572ab88f66
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2021-06-30 15:00:22 +0600

    Update context_processors.py

--- expenses/context_processors.py
+++ expenses/context_processors.py
@@ -2,10 +2,10 @@ from .models import Expense
 from django.db.models import Avg, Sum, Max, Min, Count
 
 
-# def getExpenseData(request):
+def getExpenseData(request):
 
-#     expense = Expense.objects.filter(owner=request.user).aggregate(Sum("amount"))
+    expense = Expense.objects.filter(owner=request.user).aggregate(Sum("amount"))
 
-#     context = {"totalExpense": expense["amount__sum"]}
+    context = {"totalExpense": expense["amount__sum"]}
 
-#     return context
+    return context
