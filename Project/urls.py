6070afbbdbb41c2d45f0ad05a68b3e4d2e7aca46
hossainchisty <hossain.chisty11@gmail.com>
2021-07-10 02:17:13 +0600

    add robots.txt and sitemap✨🚀

--- Project/urls.py
+++ Project/urls.py
@@ -14,24 +14,55 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
 from django.conf import settings
+from django.urls import include, path
 from django.conf.urls.static import static
+from django.contrib.sitemaps.views import sitemap
+from django.views.generic.base import TemplateView
+from django.contrib.sitemaps import GenericSitemap
 from django.conf.urls import handler400, handler403, handler404, handler500
+
+# import model
+from expenses.models import Expense
+
+
+info_dict = {
+    "queryset": Expense.objects.all(),
+    "date_field": "date",
+}
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    # Local apps URL Configuration
     path("", include("income.urls")),
     path("accounts/", include("accounts.urls")),
     path("expenses/", include("expenses.urls")),
+    # Sitemap and robots.txt URL Configuration
+    path(
+        "sitemap.xml",
+        sitemap,
+        {
+            "sitemaps": {
+                "expense": GenericSitemap(
+                    info_dict,
+                    priority=0.6,
+                    changefreq="weekly",
+                )
+            }
+        },
+        name="django.contrib.sitemaps.views.sitemap",
+    ),
+    path(
+        "robots.txt",
+        TemplateView.as_view(
+            template_name="config/robots.txt", content_type="text/plain"
+        ),
+    ),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-# hossainchisty11
-
-# pass: #DqDca]R]Z8^YdnV
 
 
-# Custome Error
-handler404 = "accounts.views.error_404"
-# handler500 = "accounts.views.error_500"
+# Customizing-error-views
+handler404 = "accounts.views.page_not_found_error"
+handler500 = "accounts.views.page_error"
 # handler403 = "accounts.views.error_403"
 # handler400 = "accounts.views.error_400"
