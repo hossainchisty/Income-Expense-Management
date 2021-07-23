8e6ac632f72409cea312592ec349365acd18e479
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2021-07-23 16:47:59 +0600

    Passed test cases🚀♻

--- Project/settings.py
+++ Project/settings.py
@@ -186,8 +186,7 @@ EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = config("MAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = config("MAIL_HOST_PASSWORD")
-
+EMAIL_HOST_PASSWORD = config("PASSWORD")
 # EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
 # CRISPY FORM
