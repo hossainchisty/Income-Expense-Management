0d6ccab2afeaf215c86cd9130dac20901de575c8
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2021-08-24 13:07:23 +0600

    write unit test(TDD)🧵

--- expenses/tests.py
+++ expenses/tests.py
@@ -1,3 +1,39 @@
 from django.test import TestCase
+from .models import Expense
 
-# Create your tests here.
+
+class ExpenseTest(TestCase):
+    """Test module for Expense model"""
+
+    def setUp(self):
+        """Set up for tests"""
+        Expense.objects.create(
+            why='Test expense',
+            amount=100,
+            description='Test description'
+        )
+    
+    def tearDown(self):
+        """Tear down for tests"""
+        Expense.objects.all().delete()
+        
+    def test_expense_str(self):
+        """Test __str__ method"""
+        expense = Expense.objects.get(why='Test expense')
+        self.assertEqual(expense.__str__(), 'Test expense')
+    
+    def test_expense_why(self):
+        """Test why property"""
+        expense = Expense.objects.get(why='Test expense')
+        self.assertEqual(expense.why, 'Test expense')
+
+    def test_expense_amount(self):
+        """Test amount property"""
+        expense = Expense.objects.get(why='Test expense')
+        self.assertEqual(expense.amount, 100)
+    
+    def test_expense_description(self):
+        """Test description property"""
+        expense = Expense.objects.get(why='Test expense')
+        self.assertEqual(expense.description, 'Test description')
+
--- income/tests.py
+++ income/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+from .models import Income
 
-# Create your tests here.
+
+class IncomeTest(TestCase):
+    """Test module for Income model"""
+
+    def setUp(self):
+        """Set up for test"""
+        Income.objects.create(description='Test income', amount=100,sources='Test sources')
+
+    def tearDown(self):
+        """Tear down for test"""
+        Income.objects.all().delete()
+
+    def test_income_name(self):
+        """Test income name"""
+        income = Income.objects.get(description='Test income')
+        self.assertEqual(income.description, 'Test income')
+
+    def test_income_amount(self):
+        """Test income amount"""
+        income = Income.objects.get(description='Test income')
+        self.assertEqual(income.amount, 100)
+
+    def test_income_sources(self):
+        """Test income sources"""
+        income = Income.objects.get(sources='Test sources')
+        self.assertEqual(income.sources, 'Test sources')
