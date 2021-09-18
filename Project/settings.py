02ddc00ec17f4ff974f2fc63dcd841db643c03cd
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2021-09-18 13:48:03 +0600

    Improve code quality✨ Lint code enforce PEP8 compliance🚀

--- Project/settings.py
+++ Project/settings.py
@@ -1,27 +1,29 @@
 """
 Title: Income Expense Manager
-Author: Hossain Chisty
 Description: Income expense manager is easiest and user friendly application to control your finance.
+Author: Hossain Chisty(Backend Developer)
 Contact: hossain.chisty11@gmail.com
+Github: https://github.com/hossainchisty
 """
-import os
 import django_heroku
-import dj_database_url
 from pathlib import Path
-from decouple import config
 import cloudinary
 import cloudinary.uploader
 import cloudinary.api
 
-cloudinary.config( cloud_name="hossainchisty", 
-                    api_key="958916513788356", 
-                    api_secret="2BaQjUoM5jHa3K6VVpbaSs_icBQ" )
+# Configure cloudinary
+cloudinary.config(cloud_name="hossainchisty", api_key="958916513788356", api_secret="2BaQjUoM5jHa3K6VVpbaSs_icBQ")
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = "_41ckb9(nmeg6*^tg5&jjf8e9s@(yfwl5dxk)1afiy0d720y!4"
 
 DEBUG = True
+
+if DEBUG is False:
+    SECURE_SSL_REDIRECT = True
+else:
+    SECURE_SSL_REDIRECT = False
 
 ALLOWED_HOSTS = ["127.0.0.1:8000", "django-income-expense-manager.herokuapp.com"]
 
@@ -58,14 +60,8 @@ SITE_ID = 2
 # local
 # SITE_ID = 3
 
-if DEBUG == False:
-    SECURE_SSL_REDIRECT = True
-
 AUTHENTICATION_BACKENDS = [
-    # Needed to login by username in Django admin, regardless of `allauth`
     "django.contrib.auth.backends.ModelBackend",
-
-    # `allauth` specific authentication methods, such as login by e-mail
     "allauth.account.auth_backends.AuthenticationBackend",
 ]
 
@@ -96,27 +92,16 @@ SOCIALACCOUNT_PROVIDERS = {
             'short_name'
         ],
         'EXCHANGE_TOKEN': True,
-        # 'LOCALE_FUNC': 'path.to.callable',
         'VERIFIED_EMAIL': False,
         'VERSION': 'v7.0',
     },
 }
-"""
-ACCOUNT_AUTHENTICATION_METHOD = 'email'
-ACCOUNT_EMAIL_REQUIRED = True
-ACCOUNT_UNIQUE_EMAIL = True
-ACCOUNT_USER_MODEL_USERNAME_FIELD = None
-ACCOUNT_USERNAME_REQUIRED = False
-"""
 
 MIDDLEWARE = [
     "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
-    # Cache middleware configuration
-    # "django.middleware.cache.UpdateCacheMiddleware",
     "django.middleware.common.CommonMiddleware",
-    # "django.middleware.cache.FetchFromCacheMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
@@ -151,29 +136,6 @@ DATABASES = {
     }
 }
 
-# Database caching
-# https://docs.djangoproject.com/en/3.2/topics/cache/
-
-# The cache timeout, in seconds
-# CACHE_MIDDLEWARE_SECONDS = 20
-
-# CACHES = {
-#     "default": {
-#         "BACKEND": "django.core.cache.backends.db.DatabaseCache",
-#         "LOCATION": "income_expenses_manager_caches",
-#     }
-# }
-
-# python manage.py createachetable
-# Filesystem caching
-
-# CACHES = {
-#     "default": {
-#         "BACKEND": "django.core.cache.backends.filebased.FileBasedCache",
-#         "LOCATION": "G:\Income and expenses\cache",
-#     }
-# }
-
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -190,8 +152,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
-
 LANGUAGE_CODE = "en-us"
 
 TIME_ZONE = "Asia/Dhaka"
@@ -203,44 +163,29 @@ USE_L10N = True
 USE_TZ = True
 
 
-
-STATIC_URL = "/static/"
-
-
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-# sTATIC FILES Configuration
+# Static files configuration
 STATICFILES_DIRS = ["static"]
-# MEDIA_URL = "/media/"
-# MEDIA_ROOT = BASE_DIR / "media"
+STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
+# Crispy Form Pack - Bootstrap 4
+CRISPY_TEMPLATE_PACK = "bootstrap4"
+
+# Mail configrations
+EMAIL_HOST = "smtp.zoho.com"
+EMAIL_PORT = 465
+EMAIL_HOST_USER = "hossain.chisty@zohomail.com"
+DEFAULT_FROM_EMAIL = "hossain.chisty@zohomail.com"
+EMAIL_HOST_PASSWORD = "#2#3B399TiU@aBC"
+EMAIL_USE_SSL = True
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
 LOGIN_REDIRECT_URL = "dashboard"
 LOGOUT_REDIRECT_URL = "/"
 ACCOUNT_LOGOUT_REDIRECT_URL = 'account_login'
 
-django_heroku.settings(locals())
-
-# SMTP Configuration
-# EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-# EMAIL_HOST = "smtp.gmail.com"
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
-# EMAIL_HOST_USER = config("MAIL_HOST_USER")
-# EMAIL_HOST_PASSWORD = config("PASSWORD")
-
-
-EMAIL_HOST = "smtp.zoho.com"
-EMAIL_PORT = 465
-EMAIL_HOST_USER = "hossain.chisty@zohomail.com"
-EMAIL_HOST_PASSWORD = "#2#3B399TiU@aBC"
-EMAIL_USE_SSL = True
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-
-# EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
-
-# AUTHENTICATION_BACKENDS = ("django.contrib.auth.backends.ModelBackend",)
-
-CRISPY_TEMPLATE_PACK = "bootstrap4"
+# Activate Django-Heroku.
+django_heroku.settings(locals(), test_runner=False)
